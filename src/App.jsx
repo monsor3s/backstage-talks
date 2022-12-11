6f6d257890eaa0a861989a7ec6c8796299f2c2a4
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2022-12-11 20:00:47 -0300

    add elements section-left

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import Logo from "../src/assets/logo.png"
+
+import "../src/styles/components/app.sass"
+
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <body>
+      <section className="main-section">
+        <div className="section-left">
+          <img className="logo" src={Logo} alt="logo" />
+          <p className="description">Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right s, we need to talk.
+          <span className="span-copyright">© 2022<a className="published">Published by studio Milk</a></span>
+          <p className="main-privacy">Privacy Policy</p>
+          </p>
+        </div>
+        <div className="section-central">
+
+        </div>
+        <div className="section-right">
+
+        </div>
+      </section>
+    </body>
   )
 }
 
