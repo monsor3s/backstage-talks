f9478e7ecd7eb6629d7a4c9aa607aaec2d528666
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2023-01-16 11:14:52 -0300

    create class animate

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,17 @@
-const target = document.querySelectorAll('issueSix');
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate'
 
-function changeBackground() {
-  const windowTop = window.pageYOffset;
-  target.forEach(function(element) {
-    console.log(element.offsetTop);
-  })
-}
+  function changeBackground() {
+    console.log('sdadsaaa')
+  }
 
 window.addEventListener('scroll', function(){
   changeBackground();
 })
+
+// function changeBackground() {
+//   const windowTop = window.pageYOffset;
+//   target.forEach(function(element) {
+//     console.log(element.offsetTop);
+//   })
+// }
